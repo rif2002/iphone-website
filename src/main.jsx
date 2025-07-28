69b12150535136a5e1584934424de0fb9e89dda7
rif2002 <147876762+rif2002@users.noreply.github.com>
2025-07-28 21:09:14 +0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://cf5d1e52c612b3cabc70bc7cc45117ec@o4509746252742656.ingest.de.sentry.io/4509746255233104",
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
